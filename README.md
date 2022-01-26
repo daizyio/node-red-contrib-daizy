4ee9c2d7d506dd5aad267878108f9af1f05a2015
Jamie Buckley <jamie.buckley@daizy.io>
2022-01-26 12:50:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Table of Contents
+- [Introduction](#introduction)
+- [Install](#install)
+- [Usage](#usage)
+- [Copyright and license](#copyright-and-license)
+
+## Introduction
+The daizy events node is a way for you to integrate your iot events into your Node Red flow, once a token is added you can receive events from any of your Daizy projects.
+
+## Install
+Run this command in the root directory of your Node Red install:
+
+`npm install node-red-contrib-daizy`
+
+## Usage
+The daizy events node works as entry node and passes a payload object for each event received, the only configuration required is a token which can be generated via the Daizy Portal.
+
+## Copyright and license
+???
---
+++ daizy-datasets.js
@@ -0,0 +1,99 @@
+let Mqtt = require('mqtt');
+
+let TOPIC_PREFIX = `/out/integrations/`
+
+module.exports = function(RED) {
+	"use strict";
+
+  RED.nodes.registerType("daizy events", DaizyDatasets, {
+		credentials : {
+		  token : { type: 'text' }
+		}
+	});
+
+	function DaizyDatasets(n) {
+    RED.nodes.createNode(this, n);
+		if (!this.connection) {
+			this.connect(n.token, n.endpoint);
+		}
+	}
+
+	DaizyDatasets.prototype.connect = function(token, endpoint) {
+    this.status(this.SUBSCRIBING);
+		if (!this.connection) {
+			try {
+        const clientId = `nodered-${token.substring(0, 8)}-${this.id}`;
+        this.log(`Connecting ${clientId}`);
+				this.connection = Mqtt.connect(endpoint, {
+					username: token,
+					password: token,
+					clientId,
+					clean: false,
+          keepalive: 180,
+          reconnectPeriod: 10
+				});
+
+        var self = this;
+
+				this.connection.on('connect', function() {
+					let devTopic = 'devices';
+					if (endpoint == 'wss://mqtt-test.daizy.io') {
+						devTopic = 'devices-dev';
+					}
+					const topic = devTopic + TOPIC_PREFIX + token;
+					self.connection.subscribe(topic, { qos: 1 });
+					self.log(`Daizy ready to go! (${clientId} subscribed to ${topic})`);
+          self.status(self.SUBSCRIBED);
+				});
+
+				this.connection.on('error', function(err) {
+					self.log('Something went wrong', err)
+					self.status(self.ERROR);
+				})
+
+				this.connection.on('close', function () {
+					self.log('Connection was closed')
+          self.status(self.ERROR)
+				})
+
+				this.connection.on('message', function(topic, payload) {
+          payload = JSON.parse(payload);
+          self.log(`Received message on ${topic}`);
+          self.send({
+            topic : topic,
+            payload : payload
+          });
+        });
+
+			} catch(e) {
+        this.log('Something went wrong', e)
+				this.status(this.ERROR)
+			}
+		}
+	};
+
+  DaizyDatasets.prototype.close = function() {
+		if(this.connection) {
+			this.log("Disconnecting")
+			this.connection.end()
+		}
+	}
+
+	DaizyDatasets.prototype.SUBSCRIBING = {
+		fill : "yellow",
+		shape : "ring",
+		text : "subscribing..."
+	}
+
+	DaizyDatasets.prototype.SUBSCRIBED = {
+		fill : "green",
+		shape : "ring",
+		text : "subscribed"
+	}
+
+	DaizyDatasets.prototype.ERROR = {
+		fill : "red",
+		shape : "ring",
+		text : "Error"
+	}
+};
